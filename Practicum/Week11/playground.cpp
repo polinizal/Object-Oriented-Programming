f65caf66de81a9b4fd1d96536c38ea7d2ceec56c
Slavi15 <slavirezashkiofficial@gmail.com>
2025-04-30 12:06:40 +0300

    Add Week11

---
+++ Practicum/Week11/playground.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+
+struct A
+{
+	virtual void f()
+	{
+		std::cout << "A::f()" << std::endl;
+	}
+
+	virtual void g()
+	{
+		std::cout << "A::g()" << std::endl;
+	}
+
+	void non_virtual_function()
+	{
+		std::cout << "A::non_virtual_function()" << std::endl;
+	}
+};
+
+struct B : public A
+{
+	void f()
+	{
+		std::cout << "B::f()" << std::endl;
+	}
+
+	void g()
+	{
+		std::cout << "B::g()" << std::endl;
+	}
+
+	void h()
+	{
+		std::cout << "B::h()" << std::endl;
+	}
+};
+
+struct C : public B
+{
+	void f()
+	{
+		std::cout << "C::f()" << std::endl;
+	}
+
+	void non_virtual_function()
+	{
+		std::cout << "C::non_virtual_function()" << std::endl;
+	}
+
+};
+
+struct D : public C
+{
+	void f()
+	{
+		std::cout << "D::f()" << std::endl;
+	}
+
+	void d()
+	{
+		std::cout << "D::d()" << std::endl;
+	}
+};
+
+int main()
+{
+	A objA;
+	B objB;
+	C objC;
+	D objD;
+
+	A* ptrA = &objA;
+
+	ptrA->f();
+	ptrA->g();
+	ptrA->non_virtual_function();
+
+	A* ptrB = &objB;
+
+	ptrB->f();
+	ptrB->g();
+	ptrB->non_virtual_function();
+
+	B* ptrC = &objC;
+
+	ptrC->f();
+	ptrC->g();
+	ptrC->non_virtual_function();
+
+	C* ptrD = &objD;
+
+	ptrD->f();
+	ptrD->g();
+	ptrD->non_virtual_function();
+}
